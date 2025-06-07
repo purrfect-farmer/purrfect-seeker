8bf05435b003665dbf1610d4c09a600cc3511a5f
Sadiq Salau <sadiqsalau888@gmail.com>
2025-06-07 07:32:26 +0100

    Allow specifying ID and used alphanumeric for key

--- commands/add-server.js
+++ commands/add-server.js
@@ -11,20 +11,26 @@ module.exports = (program, inquirer, chalk) => {
       const { default: cryptoRandomString } = require("crypto-random-string");
       const db = require("../db/models");
 
-      const { name, address, key } = await inquirer.prompt([
+      const { id, key, name, address } = await inquirer.prompt([
         { name: "name", message: "Name:", required: true },
         { name: "address", message: "Address (Optional):" },
+        { name: "id", message: "ID (Optional):" },
         { name: "key", message: "Key (Optional):" },
       ]);
 
+      /** Create Server */
       const server = await db.Server.create({
+        ...(id && { id }),
         name,
         address,
-        key: key || cryptoRandomString({ length: 16, type: "base64" }),
+        key: key || cryptoRandomString({ length: 16, type: "alphanumeric" }),
       });
 
       /** Success Message */
       console.log(chalk.bold.green("Server created successfully!"));
+
+      /** ID */
+      console.log(`${chalk.bold.blue("ID:")} ${chalk.bold.green(server.id)}`);
 
       /** Key */
       console.log(`${chalk.bold.blue("Key:")} ${chalk.bold.green(server.key)}`);
