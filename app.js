e7a7f1cec0ccd5326ccca085f0764e5cd8a8346f
Sadiq Salau <sadiqsalau888@gmail.com>
2025-06-08 16:56:06 +0100

    Disable logging in production

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-'use strict'
+"use strict";
 
-const path = require('node:path')
-const AutoLoad = require('@fastify/autoload')
+const path = require("node:path");
+const AutoLoad = require("@fastify/autoload");
 
 // Pass --options via CLI arguments in command to enable these options.
-const options = {}
+const options = {
+  logger: process.env.NODE_ENV !== "production",
+};
 
 module.exports = async function (fastify, opts) {
   // Place here your custom code!
@@ -15,16 +17,16 @@ module.exports = async function (fastify, opts) {
   // those should be support plugins that are reused
   // through your application
   fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts)
-  })
+    dir: path.join(__dirname, "plugins"),
+    options: Object.assign({}, opts),
+  });
 
   // This loads all plugins defined in routes
   // define your routes in one of these
   fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'routes'),
-    options: Object.assign({}, opts)
-  })
-}
+    dir: path.join(__dirname, "routes"),
+    options: Object.assign({}, opts),
+  });
+};
 
-module.exports.options = options
+module.exports.options = options;
--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,12 +5,13 @@ module.exports = {
     {
       name: process.env.PM2_APP_NAME ?? "purrfect-seeker",
       script: "pnpm",
-      args: "start",
+      args: "start --options",
       interpreter: "none",
       cwd: __dirname,
       env: {
         PORT: process.env.PORT ?? 3000,
         NODE_ENV: process.env.NODE_ENV ?? "production",
+        FORCE_COLOR: true,
       },
     },
   ],
